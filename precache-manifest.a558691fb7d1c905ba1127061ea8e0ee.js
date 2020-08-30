4cbf5ce9928ae804b7f7ea21c9b4c9e46796dc57
Shabnam Ahmed <shabnam.ahmed@awh.net>
2020-08-30 17:01:49 -0400

    Updates

---
+++ precache-manifest.a558691fb7d1c905ba1127061ea8e0ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed45d5d4d8183e42e40fc62ea31406fc",
+    "url": "/imdb-parental-guide/index.html"
+  },
+  {
+    "revision": "794f13817294cdcbf6d8",
+    "url": "/imdb-parental-guide/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "dff72ce9270d7529912d",
+    "url": "/imdb-parental-guide/static/js/2.f8cfe5ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imdb-parental-guide/static/js/2.f8cfe5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794f13817294cdcbf6d8",
+    "url": "/imdb-parental-guide/static/js/main.899628e1.chunk.js"
+  },
+  {
+    "revision": "bee59a4454ef26d6d29e",
+    "url": "/imdb-parental-guide/static/js/runtime-main.c187cc80.js"
+  }
+]);
